cf9cc97a5870563ac0cc0f6183dc8b5f3c54ddac
Vishal <Vishalkpal2479@gmail.com>
2022-10-08 14:15:14 +0530

    Add project files.

---
+++ Homeo_mgt/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Homeo_mgt.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Homeo_mgt.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login() 
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ Homeo_mgt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Homeo_mgt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
